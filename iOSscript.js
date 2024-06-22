8bad665ba282aa98d4f87738de46754de27f63fa
Derrick <n01535209@unf.edu>
2024-06-22 09:10:49 -0400

    Update iOSscript.js

--- iOSscript.js
+++ iOSscript.js
@@ -1,5 +1,6 @@
 let htmlCode = "";
 function openApp(){
-    htmlCode += "<div id='application'><span class='material-symbols-outlined' onclick='$(this).parent().remove();'>arrow_back_ios</span></div>";
+    htmlCode += "<div id='application'><span class='material-symbols-outlined' onclick='$(this).parent().remove();'>arrow_back_ios</span>";
+    htmlCode += "</div>";
     document.body.innerHTML += htmlCode;
 }
